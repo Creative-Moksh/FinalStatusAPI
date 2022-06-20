460c120ea4cac5b617815bfc06e8fa501b98ba36
mokshad <mokshad123@gmail.com>
2022-06-20 22:58:44 +0530

    Add project files.

---
+++ IndexStatus.aspx.cs
@@ -0,0 +1,120 @@
+﻿using CCA.Util;
+using System;
+using System.Collections.Specialized;
+using System.IO;
+using System.Net;
+
+namespace FinalStatusAPI
+{
+    public partial class IndexStatus : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                string accessCode = "";//from avenues
+                string workingKey = "";// from avenues
+
+                string orderStatusQueryJson = "{ \"order_no\": \"\" }"; //Ex. { "reference_no":"CCAvenue_Reference_No" , "order_no":"123456"} 
+                string encJson = "";
+
+                string queryUrl = "https://apitest.ccavenue.com/apis/servlet/DoWebTrans";
+
+                CCACrypto ccaCrypto = new CCACrypto();
+                encJson = ccaCrypto.Encrypt(orderStatusQueryJson, workingKey);
+
+                // make query for the status of the order to ccAvenues change the command param as per your need
+                string authQueryUrlParam = "enc_request=" + encJson + "&access_code=" + accessCode + "&command=orderStatusTracker&request_type=JSON&response_type=JSON&version=1.2";
+
+                // Url Connection
+                String message = postPaymentRequestToGateway(queryUrl, authQueryUrlParam);
+                //Response.Write(message);
+                NameValueCollection param = getResponseMap(message);
+                String status = "";
+                String encResJson = "";
+                if (param != null && param.Count == 2)
+                {
+                    for (int i = 0; i < param.Count; i++)
+                    {
+                        if ("status".Equals(param.Keys[i]))
+                        {
+                            status = param[i];
+                        }
+                        if ("enc_response".Equals(param.Keys[i]))
+                        {
+                            encResJson = param[i];
+                            //Response.Write(encResXML);
+                        }
+                    }
+                    if (!"".Equals(status) && status.Equals("0"))
+                    {
+                        String ResJson = ccaCrypto.Decrypt(encResJson, workingKey);
+                        Response.Write(ResJson);
+                    }
+                    else if (!"".Equals(status) && status.Equals("1"))
+                    {
+                        Console.WriteLine("failure response from ccAvenues: " + encResJson);
+                    }
+
+                }
+
+            }
+            catch (Exception exp)
+            {
+                Response.Write("Exception " + exp);
+
+            }
+        }
+
+        private string postPaymentRequestToGateway(String queryUrl, String urlParam)
+        {
+
+            String message = "";
+            try
+            {
+                StreamWriter myWriter = null;// it will open a http connection with provided url
+                WebRequest objRequest = WebRequest.Create(queryUrl);//send data using objxmlhttp object
+                objRequest.Method = "POST";
+                //objRequest.ContentLength = TranRequest.Length;
+                objRequest.ContentType = "application/x-www-form-urlencoded";//to set content type
+                myWriter = new System.IO.StreamWriter(objRequest.GetRequestStream());
+                myWriter.Write(urlParam);//send data
+                myWriter.Close();//closed the myWriter object
+
+                // Getting Response
+                System.Net.HttpWebResponse objResponse = (System.Net.HttpWebResponse)objRequest.GetResponse();//receive the responce from objxmlhttp object 
+                using (System.IO.StreamReader sr = new System.IO.StreamReader(objResponse.GetResponseStream()))
+                {
+                    message = sr.ReadToEnd();
+                    //Response.Write(message);
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.Write("Exception occured while connection." + exception);
+            }
+            return message;
+
+        }
+
+        private NameValueCollection getResponseMap(String message)
+        {
+            NameValueCollection Params = new NameValueCollection();
+            if (message != null || !"".Equals(message))
+            {
+                string[] segments = message.Split('&');
+                foreach (string seg in segments)
+                {
+                    string[] parts = seg.Split('=');
+                    if (parts.Length > 0)
+                    {
+                        string Key = parts[0].Trim();
+                        string Value = parts[1].Trim();
+                        Params.Add(Key, Value);
+                    }
+                }
+            }
+            return Params;
+        }
+    }
+}
